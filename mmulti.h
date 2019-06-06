e673b760ff2dfa1aeb705b040372131df15ecbb9
Sylvio Correa <sylvio.acad@acad.pucrs.br>
2019-06-06 17:45:43 -0300

    another push

--- mmulti.h
+++ mmulti.h
@@ -1,6 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "mpi.h"
 
 
+void naive_multi(int *A, int *B, int *C, int size);
+void matrix_alloc(int **ptr, int size);
+void print_matrix(int *M, int size);
+void msum(int *A, int *B, int *C, int cl, int cc, int size_ab);
+void mmulti(int *A, int *B,
+            int al, int ac,
+            int bl, int bc,
+            int *C, int s, int size);
